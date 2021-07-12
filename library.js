a7a212e3acad809faebfbf6aa360cc7e13a6084b
mosky <smartmosky@gmail.com>
2021-07-11 22:22:24 -0400

    Added UI, but there's a glitch where the first book info does not display

--- library.js
+++ library.js
@@ -1,11 +1,12 @@
+let display = document.querySelector('#Display');
 let addButton = document.querySelector('#add');
 let myLibrary = [];
-
+let formHolder = document.querySelector('#formHolder');
 function Book(name, author, pages, read) {
     this.name = name
     this.author = author
     this.pages = pages
-    this.read = read
+    this.read = read 
    this.info = function() {
        return `${name} by ${author}, ${pages} pages, ${read}`}     
   };
@@ -15,15 +16,32 @@ myLibrary.push(book)
 };
 
 function bookDisplay() {
+    let bookName = document.createElement('div');
+let authorName = document.createElement('div');
+let pageNum = document.createElement('div');
+let readYet = document.createElement('div');
     for (let i = 0; i < myLibrary.length; i++) {
-console.log(myLibrary[i].info())
+let book = document.createElement('div');
+book.appendChild(bookName);
+bookName.textContent = `${myLibrary[i].name}`;
+book.appendChild(authorName);
+authorName.textContent = `${myLibrary[i].author}`;
+book.appendChild(pageNum);
+pageNum.textContent = `${myLibrary[i].pages} pages`;
+book.appendChild(readYet);
+readYet.textContent = `${myLibrary[i].read}`;
+display.appendChild(book);
+book.classList.add('book');
+;
     }
 };
 
-addButton.addEventListener ('click', () => {});
+addButton.addEventListener ('click', () => {
 
-let theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295', 'not yet read');
-let fantasticMrFox = new Book('Fantastic Mr. Fox', 'Roald Dahl', '96', 'not yet read');
+});
+
+let theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295', 'Not yet read');
+let fantasticMrFox = new Book('Fantastic Mr. Fox', 'Roald Dahl', '96', 'Not yet read');
 
 addBookToLibrary(theHobbit);
 addBookToLibrary(fantasticMrFox);
